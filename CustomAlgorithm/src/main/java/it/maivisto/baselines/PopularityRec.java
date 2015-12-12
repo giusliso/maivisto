6eb31e02821a1897a01eb2f07777661630538bc6
lucanar <lucanar@users.noreply.github.com>
2015-12-12 18:53:34 +0100

    Update PopularityRec.java

--- CustomAlgorithm/src/main/java/it/maivisto/baselines/PopularityRec.java
+++ CustomAlgorithm/src/main/java/it/maivisto/baselines/PopularityRec.java
@@ -1,8 +1,8 @@
 package it.maivisto.baselines;
 
+import java.util.HashSet;
 import java.util.List;
-import java.util.TreeSet;
-
+import java.util.Random;
 import javax.inject.Inject;
 
 import org.grouplens.lenskit.ItemScorer;
@@ -20,11 +20,12 @@ import org.slf4j.LoggerFactory;
 import it.unimi.dsi.fastutil.longs.LongSet;
 
 /**
- * Popularity Recommender. 
- * It recommends the list of the 'n' most popular items, i.e. those with the highest number of ratings.
+ * Random Popularity Recommender. 
+ * It extends the list of 'n-1' randomly selected items by inserting the most popular item, 
+ * i.e. the item with the highest number of ratings.
  */
-public class PopularityRec extends AbstractItemRecommender {
-	private static final Logger logger = LoggerFactory.getLogger(PopularityRec.class);
+public class RandomPopularityRec extends AbstractItemRecommender {
+	private static final Logger logger = LoggerFactory.getLogger(RandomPopularityRec.class);
 
 	private ItemDAO idao;
 	private ItemEventDAO iedao;
@@ -32,7 +33,7 @@ public class PopularityRec extends AbstractItemRecommender {
 	private ItemScorer scorer;
 
 	@Inject
-	public PopularityRec(ItemDAO idao, UserEventDAO uedao, ItemEventDAO iedao, ItemScorer scorer) {
+	public RandomPopularityRec(ItemDAO idao, UserEventDAO uedao, ItemEventDAO iedao, ItemScorer scorer) {
 		this.uedao = uedao;
 		this.idao = idao;
 		this.iedao = iedao;
@@ -59,72 +60,36 @@ public class PopularityRec extends AbstractItemRecommender {
 			for(Rating rate : userHistory)
 				recommendableItems.remove(rate.getItemId());		
 
-		TreeSet<Popularity> popTree = new TreeSet<>(); // items sorted by popularity
-		for(Long itemId : recommendableItems)
-			popTree.add(new Popularity(itemId, iedao.getEventsForItem(itemId).size()));
-		logger.info("Sorted {} items by popularity", recommendableItems.size());
-
-		// add n items to the recommendations list
-		int lastPop=0;
-		int count = 0;
-		for(Popularity pop : popTree) {
-			if(count!=n){
-				reclist.put(pop.getItem(),scorer.score(user, pop.getItem()));
-				lastPop = pop.getPopularity();
-				count++;
+		// add the most popular item to recommendation list
+		long mostPopularItem=-1;
+		int maxPopularity=0;
+		for(Long itemId : recommendableItems){
+			int pop = iedao.getEventsForItem(itemId).size();
+			if(pop>maxPopularity){
+				mostPopularItem=itemId;
+				maxPopularity=pop;
 			}
-			else
-				if(pop.getPopularity() == lastPop)
-					reclist.put(pop.getItem(),scorer.score(user, pop.getItem()));
-				else
-					break;
+		}
+		logger.info("Added most popular item {}", mostPopularItem);
+		recommendableItems.remove(mostPopularItem);
+		reclist.put(mostPopularItem, scorer.score(user, mostPopularItem));
+
+		// add n-1 randomly selected items to recommendation list
+		logger.info("Randomly selecting {} items", n-1);
+		Random random = new Random();
+		Object[] recItems = recommendableItems.toArray();	
+		HashSet<Integer> chosenItem = new HashSet<Integer>();
+		while(chosenItem.size() != n-1){
+			int i = random.nextInt(recItems.length);
+			if(!chosenItem.contains(i)){
+				chosenItem.add(i);
+				long item = (long) recItems[i];
+				logger.info("Added randomly selected item {}", item);
+				reclist.put(item, scorer.score(user, item));
+			}
 		}
 
-		return reclist.finish();
+		return reclist.finish(); 
 	}
 
-	/**
-	 * Inner class that stores information about the popularity of an item.
-	 * It stores a couple of values < itemID , number of ratings >.
-	 */
-	private class Popularity implements Comparable<Popularity> {
-		private final int popularity;
-		private final long item;
-
-		public Popularity(long item, int popularity) {
-			this.item=item;
-			this.popularity=popularity;
-		}
-
-		public int getPopularity() {
-			return popularity;
-		}
-
-		public long getItem() {
-			return item;
-		}
-
-		@Override
-		public String toString() {
-			return "[item: "+item+", popularity: "+popularity+"]";
-		}
-
-		/**
-		 * Sorts values by popularity and then by item
-		 */
-		@Override
-		public int compareTo(Popularity o) {
-			if(popularity < o.getPopularity())
-				return 1;
-			else if(popularity > o.getPopularity())
-				return -1;
-			else if(item < o.getItem())
-				return 1;
-			else if (item > o.getItem())
-				return -1;
-			else
-				return 0;
-		}
-
-	}
 }
