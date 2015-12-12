15e0dd2929c9021eb53fcc09e5aadfb0c60cd093
lucanar <lucanar@users.noreply.github.com>
2015-12-12 18:50:35 +0100

    Update Serializer.java

--- CustomAlgorithm/src/main/java/it/maivisto/utility/Serializer.java
+++ CustomAlgorithm/src/main/java/it/maivisto/utility/Serializer.java
@@ -14,26 +14,29 @@ import com.esotericsoftware.kryo.io.Output;
 public class Serializer {
 
 	private static final Logger logger = LoggerFactory.getLogger(Serializer.class);
-
+	private Kryo kryo=null;
+	
 	/**
 	 * Serialize an object
 	 * @param dirPath directory in which serialize the object
 	 * @param obj object to serialize
 	 * @param filename name of the file
 	 */
+	public Serializer(){
+		 kryo = new Kryo();
+	}
+	
 	public void serialize(String dirPath, Object obj, String filename){
 		logger.info("Serializing "+filename+"...");
 		try {
 			File dir = new File(dirPath);
 			dir.mkdir();
-			Kryo kryo = new Kryo();
-			kryo.register(obj.getClass());
 			Output output = new Output(new FileOutputStream(dir+File.separator+filename+".dat"));
 			kryo.writeObject(output, obj);
 			output.close();	
 			logger.info("Serialized "+filename);
 		} catch (IOException e) {
-			logger.error(e.getStackTrace().toString());
+			logger.error(e.getMessage());
 		}
 	}
 
@@ -41,20 +44,29 @@ public class Serializer {
 	 * Deserialize an object.
 	 * @param dirPath directory containing the file
 	 * @param filename name of the file
+	 * @param class of the object to deserialize
 	 */
 	public <T> Object deserialize(String dirPath, String filename,Class<T> cl){
 		logger.info("Deserializing "+filename+"...");
 		try {
-			Kryo kryo = new Kryo();
-			kryo.register(cl);
 			Input input = new Input(new FileInputStream(dirPath+filename+".dat"));
-			Object m=kryo.readObject(input, cl);
+			Object m=kryo.readObject(input,cl);
 			input.close();
 			logger.info("Deserialized "+filename);
 			return m;
 		} catch (Exception e) {
-			logger.error(e.getStackTrace().toString());
+			logger.error(e.getMessage());
 			return null;
-		} 		
-	}	
+		} 
+		
+	}
+
+	/**
+	 * Register a not serializable class to serialize
+	 * @param cl class not serializable
+	 */
+	public <T> void register(Class<T> cl) {
+		kryo.register(cl);
+	}
+	
 }
